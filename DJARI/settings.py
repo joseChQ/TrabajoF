8aab269fdbafe915af2b759cdf6ea99d390857c7
joseChQ <63762044+joseChQ@users.noreply.github.com>
2020-10-05 20:50:12 -0500

    lol
    lol

--- DJARI/settings.py
+++ DJARI/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'caja',
-    'inscripciones',# new
+    'inscripciones',
+    'configuraciones',# new
 ]
 
 MIDDLEWARE = [
